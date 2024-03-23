1646115b96139739b3efc9f5eadc05fd5a09f7e6
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-03-23 11:56:41 +0530

    Initial Commit

---
+++ SFSymbols/SFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsApp.swift
+//  SFSymbols
+//
+//  Created by mac on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
